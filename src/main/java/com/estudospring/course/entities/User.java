6cd75dfb517aa3faa970ca70dc9eac6bf4c36b19
Lyra <nmlyra13@gmail.com>
2025-06-22 19:18:02 -0300

    H2 database, test profile, JPA

--- src/main/java/com/estudospring/course/entities/User.java
+++ src/main/java/com/estudospring/course/entities/User.java
@@ -3,8 +3,21 @@ package com.estudospring.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	// @Id annotation that announces for the dice bank that it is a primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// @GeneratedValue annotation announcing for dice bank that Id is autoincrement
 	private Long id;
 	private String name;
 	private String email;
